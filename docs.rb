cfd6cac7eeacdf5ddcebed63e45514335eff5165
anyangocynthia <acynthiaanyango@gmail.com>
2016-06-23 12:40:09 +0300

    added pandoc

--- docs.rb
+++ docs.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+require 'pandoc-ruby'
+require 'asciidoctor'
 
 # gets all folders
 def self.get_folders
- Dir.entries('.').select {|entry| File.directory? File.join('.',entry ) and !(entry =='.' || entry == '..') }
+ Dir.entries(".").select {|entry| File.directory? File.join(".",entry ) and !(entry =="." || entry == "..") }
 end
 
 # selects folders with plugins
@@ -11,18 +13,23 @@ def self.plugins
 end
 
 # gets readme files in the folders
-def self.get_readmes
-	c = File.open("docs.md", "w")
+def self.get
+	c = File.open("index.adoc", "w")
 	get_folders = plugins
 	get_folders.each do |folder|
-		Dir.glob('./' + folder + '/*.md') do |md|
-			puts md
-			puts %(md\n=============)
-			f = File.open(md, "r") 
+		# Dir.glob.select('./' + folder + '/*plugin') do |adoc|
+		Dir.glob('./' + folder + '/*.adoc') do |adoc|
+			puts adoc
+			puts %(adoc\n=============)
+			f = File.open(adoc, "r") 
 				f.each_line do |line|
-					c.write line
+					c.write line						
+					end
 				end
 			end 
-		end 
+		# end
 			puts '============='
+
+
+Asciidoctor.convert_file 'index.adoc', to_file: true, safe: :safe
 end
