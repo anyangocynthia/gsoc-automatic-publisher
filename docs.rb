4a18968ef5faa806c247603ed549f3ad74c390da
anyangocynthia <acynthiaanyango@gmail.com>
2016-06-23 16:11:32 +0300

    removed self from method names

--- docs.rb
+++ docs.rb
@@ -3,22 +3,22 @@ require 'pandoc-ruby'
 require 'asciidoctor'
 
 # gets all folders
-def self.get_folders
+def get_folders
  Dir.entries(".").select {|entry| File.directory? File.join(".",entry ) and !(entry =="." || entry == "..") }
 end
 
 # selects folders with plugins
-def self.plugins
+def plugins
   get_folders.select {|entry| entry.match(/plugin/)}
 end
 
 # gets documentation files in the folders
-def self.get_docs
+def get_docs
 	c = File.open("index.adoc", "w")
 	get_folders = plugins
 	get_folders.each do |folder|
 		# Dir.glob.select('./' + folder + '/*plugin') do |adoc|
-		Dir.glob('./' + folder + '/*.adoc') do |adoc|
+		Dir.glob('./' + folder + '/*.adoc' || './' + folder + '/*.md' ) do |adoc|
 			puts adoc
 			puts %(adoc\n=============)
 			f = File.open(adoc, "r") 
